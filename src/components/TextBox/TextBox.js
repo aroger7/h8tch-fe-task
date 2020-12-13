73da38d474d88b26b9a84094cca54f4bd1b2890a
Adam Rogers <adam@adamrogers.ca>
2020-12-13 18:19:56 -0500

    add TextBox component

---
+++ src/components/TextBox/TextBox.js
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+import { colors } from 'styles';
+
+const TextBox = styled.input`
+  border: 4px solid ${colors.white};
+  background: #9AA0AB;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 1.5rem 1rem;
+  color: ${colors.white};
+
+  &::placeholder {
+    color: #D1D3D8;
+    text-transform: uppercase;
+  }
+`;
+
+export default TextBox;
