fc47df9fba18f73d92d2b5c1377bf363820fe25d
Adam Rogers <adam@adamrogers.ca>
2020-12-15 13:01:07 -0500

    adjust App styling

--- src/components/App/App.styles.js
+++ src/components/App/App.styles.js
@@ -4,12 +4,17 @@ import { colors } from 'styles';
 export const App = styled.div`
   background-color: ${colors.primary};
   width: 100%;
-  height: 100%;
+  min-height: 100%;
 `;
 
 export const AppBody = styled.div`
   margin: 0 auto;
-  padding-top: 7rem;
+  padding: 7rem 2rem 3rem;
+  box-sizing: border-box;
   width: 100%;
   max-width: 59rem;
+
+  @media screen and (max-width: 48em) {
+    padding-top: 5rem;
+  }
 `;
