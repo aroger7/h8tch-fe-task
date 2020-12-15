8b9dc878456d8508abe7198b86314e1990589cb1
Adam Rogers <adam@adamrogers.ca>
2020-12-15 12:58:13 -0500

    add default merriweather font to elements

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -2,4 +2,8 @@ import { css } from '@emotion/react';
 
 export const fonts = css`
   @import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;1,300&display=swap');
+
+  h1, h2, h3, h4, h5, h6, p, a, button, label, input {
+    font-family: 'Merriweather', serif;
+  }
 `;
