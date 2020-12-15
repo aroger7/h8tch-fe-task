a207475fbada9879f9f9ed1efb1f1cb36a8f93e2
Adam Rogers <adam@adamrogers.ca>
2020-12-15 12:56:17 -0500

    add useOnClickOutside hook

---
+++ src/hooks/useOnClickOutside.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+export default (ref, handler) => {
+  useEffect(() => {
+    const listener = (evt) => {
+      if (!ref.current || ref.current.contains(evt.target)) {
+        return;
+      }
+
+      handler(evt);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    }
+  }, [ref, handler]);
+};
