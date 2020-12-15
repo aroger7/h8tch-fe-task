b19579c423084977e90d7d1fa97efe550db0b57f
Adam Rogers <adam@adamrogers.ca>
2020-12-15 13:10:51 -0500

    add responsive style to Intro

--- src/components/Intro/Intro.styles.js
+++ src/components/Intro/Intro.styles.js
@@ -3,6 +3,10 @@ import { colors } from 'styles';
 
 export const Intro = styled.div`
   margin-bottom: 4.5rem;
+
+  @media screen and (max-width: 48em) {
+    margin-bottom: 2rem;
+  }
 `;
 
 export const Title = styled.h1`
@@ -11,10 +15,19 @@ export const Title = styled.h1`
   font-family: 'Merriweather', serif;
   font-style: italic;
   margin-bottom: 2rem;
+
+  @media screen and (max-width: 48em) {
+    font-size: 3rem;
+    margin-bottom: 1.5rem;
+  }
 `;
 
 export const Description = styled.p`
   font-size: 1.5rem;
   color: ${colors.aquamarine};
   max-width: 34.5rem;
+
+  @media screen and (max-width: 48em) {
+    font-size: 1.125rem;
+  }
 `;
